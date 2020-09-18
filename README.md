40b8f74932c046aed13f0b4d51a9789fb592387b
ElectrifyPro <ElectrifyPro@users.noreply.github.com>
2020-09-18 00:50:47 -0500

    Begin following structure of javascript's native array

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # mini-array2d
-A simple and light two-dimensional array implementation.
+A two-dimensional array implementation.
 
 ## Example:
 ```javascript
-var Array2D = require('mini-array2d');
+const Array2D = require('mini-array2d');
 
-var foo = new Array2D(2, 2);
-foo.fill(random());
+const foo = new Array2D(2, 2);
+foo.forEach(function(e, pos, arr) {
+	arr.set(pos[0], pos[1], pos);
+});
+
 console.log(foo.get(0, 0)); // get value at point (0, 0)
 console.log(Array2D.toDualArray(foo)); // convert Array2 to array-of-arrays
 ```
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+function clamp(v, l, r) {
+	if (v < l) {
+		return l;
+	} else if (v > r) {
+		return r;
+	} else {
+		return v;
+	}
+}
+
 /**
  * Representation of a two-dimensional array.
- * @property {number} this.lX - Length of the 2D array's x dimension.
- * @property {number} this.lY - Length of the 2D array's y dimension.
+ * @property {number} this.length - Length of the 2D array's dimensions.
  * @property {number} this.size - Amount of defined elements in the 2D array.
  * @class
  */
-class Array2DD {
+class Array2D {
 	#map;
-
+	#lX;
+	#lY;
+	
 	/**
 	 * Represents a two-dimensional array.
 	 * @param {number} lX - Length of the 2D array's x dimension.
@@ -16,33 +27,41 @@ class Array2DD {
 	 */
 	constructor(lX, lY) {
 		this.#map = new Array(lY);
-
-		for (var i = 0; i < lY; ++i) {
+		
+		for (let i = 0; i < lY; ++i) {
 			this.#map[i] = new Array(lX);
 		}
-
-		this.lX = lX;
-		this.lY = lY;
+		
+		this.#lX = lX;
+		this.#lY = lY;
 	}
-
+	
+	/**
+	 * The dimensions of the 2D array.
+	 * @type {Object}
+	 */
+	get length() {
+		return {x: this.#lX, y: this.#lY};
+	}
+	
 	/**
 	 * Amount of defined elements in the 2D array.
 	 * @type {number}
 	 */
 	get size() {
-		var count = 0;
-
-		for (var i = 0; i < this.lX; ++i) {
-			for (var j = 0; j < this.lY; ++j) {
+		let count = 0;
+		
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
 				if (this.#map[j][i] !== undefined) {
 					++count;
 				}
 			}
 		}
-
+		
 		return count;
 	}
-
+	
 	/**
 	 * Returns the element at the point (x, y) in the 2D array.
 	 * @param {number} x - X position of the element to get.
@@ -52,7 +71,7 @@ class Array2DD {
 	get(x, y) {
 		return this.#map[y][x];
 	}
-
+	
 	/**
 	 * Sets the element at the point (x, y) in the 2D array with the value `v`, returning the value that was previously there.
 	 * @param {number} x - X position of the element to set.
@@ -61,52 +80,207 @@ class Array2DD {
 	 * @type {*}
 	 */
 	set(x, y, v) {
-		var old = this.#map[y][x];
+		const old = this.#map[y][x];
 		this.#map[y][x] = v;
 		return old;
 	}
-
+	
+	/**
+	 * Inserts empty columns or removes columns from the 2D array.
+	 * @param {number} count - Number of columns to add / remove.
+	 * @param {number=1} side - Whether to modify the right or left of the 2D array. A value of 1 means that columns will be added / removed to / from the right of the array, while 0 means columns will be added / removed to / from the left.
+	 */
+	insertColumn(count, side = 1) {
+		if (count) {
+			if (side) {
+				if (count > 0) { // insert columns on right
+					for (let i = 0; i < this.#lY; ++i) {
+						this.#map[i].length += count;
+					}
+				} else if (count < 0) { // remove columns from right
+					for (let i = 0; i < this.#lY; ++i) {
+						this.#map[i].splice(clamp(this.#lX + count, 0, this.#lX), -count);
+					}
+				}
+			} else {
+				if (count > 0) { // insert columns on left
+					for (let i = 0; i < this.#lY; ++i) {
+						this.#map[i].unshift(...new Array(count));
+					}
+				} else if (count < 0) { // remove columns from left
+					for (let i = 0; i < this.#lY; ++i) {
+						this.#map[i].splice(0, -count);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Inserts empty rows or removes rows from the 2D array.
+	 * @param {number} count - Number of rows to add / remove.
+	 * @param {number=1} side - Whether to modify the top or bottom of the 2D array. A value of 1 means that rows will be added / removed to / from the bottom of the array, while 0 means rows will be added / removed to / from the top.
+	 */
+	insertRow(count, side = 1) {
+		if (count) {
+			if (side) {
+				if (count > 0) { // insert rows on bottom
+					for (let i = 0; i < count; ++i) {
+						this.#map.push(new Array(this.#lX));
+					}
+				} else if (count < 0) { // remove rows from bottom
+					this.#map.splice(clamp(this.#lY + count, 0, this.#lY), -count);
+				}
+			} else {
+				if (count > 0) { // insert rows on top
+					for (let i = 0; i < count; ++i) {
+						this.#map.unshift(new Array(this.#lX));
+					}
+				} else if (count < 0) { // remove rows from top
+					this.#map.splice(0, -count);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns true if all elements in the 2D array pass the test specified by the callback function.
+	 * @param {Array2D~callback} callback - The function that will be called on each value of the 2D array.
+	 * @type {*}
+	 */
+	every(callback) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
+				if (!callback(this.#map[j][i], [i, j], this)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 	/**
 	 * Fills the entire 2D array with the value `v`.
 	 * @param {*} v - The value to fill the array with.
 	 */
 	fill(v) {
-		for (var i = 0; i < this.lX; ++i) {
-			for (var j = 0; j < this.lY; ++j) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
 				this.#map[j][i] = v;
 			}
 		}
 	}
-
+	
+	/**
+	 * Returns a new 2D array with all the elements that passed the test implemented by the callback function.
+	 * @param {Array2D~callback} callback - The function that will be called on each value of the 2D array.
+	 * @type {Array2D}
+	 */
+	filter(callback) {
+		let min = {right: 0, bottom: 0};
+		const result = new Array2D(this.#lX, this.#lY);
+		
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
+				if (callback(this.#map[j][i], [i, j], this)) {
+					result.#map[j][i] = this.#map[j][i];
+					
+					if (i > min.right) {
+						min.right = i;
+					}
+					
+					if (j > min.bottom) {
+						min.bottom = j;
+					}
+				}
+			}
+		}
+		
+		return result.resize(min.right + 1, min.bottom + 1);
+	}
+	
 	/**
 	 * Finds and returns the first element found that passes the provided testing function. If no satifactory value is found, `undefined` is returned.
-	 * @param {Array2D~find} callback - The function that will be called on each value of the 2D array.
+	 * @param {Array2D~callback} callback - The function that will be called on each value of the 2D array.
 	 * @type {*}
 	 */
 	find(callback) {
-		for (var i = 0; i < this.lX; ++i) {
-			for (var j = 0; j < this.lY; ++j) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
+				if (callback(this.#map[j][i], [i, j], this)) {
+					return this.#map[j][i];
+				}
+			}
+		}
+		
+		return undefined;
+	}
+	
+	/**
+	 * Finds and returns the position of first element found that passes the provided testing function. If no satifactory value is found, [-1, -1] is returned.
+	 * @param {Array2D~callback} callback - The function that will be called on each value of the 2D array.
+	 * @type {Array.<number>}
+	 */
+	findPos(callback) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
 				if (callback(this.#map[j][i], [i, j], this)) {
 					return [i, j];
 				}
 			}
 		}
-
-		return undefined;
+		
+		return [-1, -1];
 	}
-
+	
 	/**
 	 * Executes a callback function on every element in the 2D array.
-	 * @param {Array2D~forEach} callback - The function that will be called on each value of the 2D array.
+	 * @param {Array2D~callback} callback - The function that will be called on each value of the 2D array.
 	 */
 	forEach(callback) {
-		for (var i = 0; i < this.lX; ++i) {
-			for (var j = 0; j < this.lY; ++j) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
 				callback(this.#map[j][i], [i, j], this);
 			}
 		}
 	}
-
+	
+	/**
+	 * Returns true if the specified value exists in the 2D array.
+	 * @param {*} v - The value to search for.
+	 * @type {boolean}
+	 */
+	includes(v) {
+		for (let i = 0; i < this.#lX; ++i) {
+			for (let j = 0; j < this.#lY; ++j) {
+				if (v === this.#map[j][i]) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Returns a copy of the 2D array whose dimensions are `x` and `y` accordingly, splicing any elements that occur outside the range. If `x` or `y` is larger than the current array's dimensions, they will be set to the current array's dimensions.
+	 * @param {number} x - The x dimension of the resized 2D array.
+	 * @param {number} y - The y dimension of the resized 2D array.
+	 * @type {Array2D}
+	 */
+	resize(x, y) {
+		const result = new Array2D(clamp(x, 0, this.#lX), clamp(y, 0, this.#lY));
+		
+		for (let i = 0; i < x; ++i) {
+			for (let j = 0; j < y; ++j) {
+				result.#map[j][i] = this.#map[j][i];
+			}
+		}
+		
+		return result;
+	}
+	
 	/**
 	 * Returns true if the point provided is within the 2D array's boundaries.
 	 * @param {number} x - X coordinate of point.
@@ -114,9 +288,19 @@ class Array2DD {
 	 * @type {boolean}
 	 */
 	within(x, y) {
-		return x >= 0 && x <= this.lX - 1 && y >= 0 && y <= this.lY - 1;
+		return x >= 0 && x <= this.#lX - 1 && y >= 0 && y <= this.#lY - 1;
 	}
-
+	
+	/**
+	 * Returns true if the value is an instance of the Array2D class.
+	 * @param {*} array2 - The value to check.
+	 * @type {boolean}
+	 * @static
+	 */
+	static isArray2D(value) {
+		return value instanceof Array2D;
+	}
+	
 	/**
 	 * Converts a 2D array to a traditional array-of-arrays.
 	 * @param {Array2D} array2 - The 2D array to convert.
@@ -126,7 +310,7 @@ class Array2DD {
 	static toDualArray(array2) {
 		return array2.#map;
 	}
-
+	
 	/**
 	 * Converts a traditional array-of-arrays to a 2D array.
 	 * @param {Array.<Array.<*>>} dualArray - Array-of-array to convert.
@@ -134,40 +318,22 @@ class Array2DD {
 	 * @static
 	 */
 	static toArray2D(dualArray) {
-		var array2 = new Array2D(dualArray[0].length, dualArray.length);
+		const array2 = new Array2D(dualArray[0].length, dualArray.length);
 		array2.#map = dualArray;
 		return array2;
 	}
-
+	
 	toString() {
-		var str = '';
-
-		for (var j = 0; j < this.lY; ++j) {
-			for (var i = 0; i < this.lX; ++i) {
-				str += this.#map[j][i] === undefined ? '.' : 'F';
-			}
-
-			if (j !== this.lY - 1) {
-				str += '\n';
-			}
-		}
-
-		return str;
+		return '[object Array2D]';
 	}
 }
 
 /**
- * @callback Array2D~find
+ * @callback Array2D~callback
  * @param {*} element - The current element being checked.
  * @param {Array.<number>} position - Position of the current element being checked in the 2D array.
  * @param {Array2D} map - Reference to the 2D array instance being checked.
  * @type {boolean}
- */
-/**
- * @callback Array2D~forEach
- * @param {*} element - The current element being checked.
- * @param {Array.<number>} position - Position of the current element being checked in the 2D array.
- * @param {Array2D} map - Reference to the 2D array instance being checked.
  */
 
 module.exports = Array2D;
